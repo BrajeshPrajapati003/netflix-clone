227395671657c21bdc547dc079802b4fdce4b7cb
BrajeshPrajapati003 <prajapatibrajesh003@gmail.com>
2025-08-08 21:52:46 +0530

    API work done

--- netflix/netflix-clone/src/components/TitleCards/TitleCards.jsx
+++ netflix/netflix-clone/src/components/TitleCards/TitleCards.jsx
@@ -1,34 +1,56 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
 
 const TitleCards = ({title, category}) => {
 
-  const cardsRef = useRef();
+    const [apiData, setApiData] = useState([]);
+    const cardsRef = useRef();
 
-  const handleWheel = (event) =>{
-    event.preventDefault;
-    cardsRef.current.scrollLeft += event.deltaY;
-  }
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MGY5MDc3NzFmYTgyNGFmMWNiYzlhOWJiNmRlNjQxOSIsIm5iZiI6MTc1NDY0NDM5My4zMTgsInN1YiI6IjY4OTViZmE5OTY1OWMyMjM2YWEzMzBlYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gZ6Prx_Yw3d2nTgGDPOJqsgsVFUfU-Pb5mwjUbkN8-I'
+      }
+    };
+
+    
+    const handleWheel = (event) =>{
+      event.preventDefault();
+      cardsRef.current.scrollLeft += event.deltaY;
+    }
 
   useEffect(()=>{
-    cardsRef.current.addEventListener('wheel', handleWheel);
+    fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+    .then(res => res.json())
+    .then(res => setApiData(res.results || []))
+    .catch(err => console.error(err));
+
+    const currentRef = cardsRef.current;
+    currentRef.addEventListener('wheel', handleWheel);
+
+    return () => currentRef.removeEventListener('wheel', handleWheel);
   }, [])
 
   return (
     <div className='title-cards'>
       <h2>{title ? title : "Popular on Netflix"} </h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index) =>{
+        {apiData.map((card, index) =>{
           return <div className="card" key={index}>
-            <img src={card.image} alt="" />
-            <p>{card.name}</p>
+
+            <img src={
+              card.backdrop_path ? `https://image.tmdb.org/t/p/w500`+card.backdrop_path : 'assets/fallback.png' } alt = {card.original_title}
+            />
+
+            <p>{card.original_title}</p>
           </div>
         })}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default TitleCards
