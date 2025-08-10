c4d610dfa3f0a402f6053387f258ab5baecd8296
BrajeshPrajapati003 <prajapatibrajesh003@gmail.com>
2025-08-10 17:40:00 +0530

    Fixed the data fetching through API

--- netflix/netflix-clone/src/components/TitleCards/TitleCards.jsx
+++ netflix/netflix-clone/src/components/TitleCards/TitleCards.jsx
@@ -3,30 +3,53 @@ import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
 
-const TitleCards = ({title, category}) => {
+const TitleCards = ({ title, category }) => {
 
-    const [apiData, setApiData] = useState([]);
-    const cardsRef = useRef();
+  const [apiData, setApiData] = useState([]);
+  const cardsRef = useRef();
 
-    const options = {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MGY5MDc3NzFmYTgyNGFmMWNiYzlhOWJiNmRlNjQxOSIsIm5iZiI6MTc1NDY0NDM5My4zMTgsInN1YiI6IjY4OTViZmE5OTY1OWMyMjM2YWEzMzBlYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gZ6Prx_Yw3d2nTgGDPOJqsgsVFUfU-Pb5mwjUbkN8-I'
-      }
-    };
-
-    
-    const handleWheel = (event) =>{
-      event.preventDefault();
-      cardsRef.current.scrollLeft += event.deltaY;
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MGY5MDc3NzFmYTgyNGFmMWNiYzlhOWJiNmRlNjQxOSIsIm5iZiI6MTc1NDY0NDM5My4zMTgsInN1YiI6IjY4OTViZmE5OTY1OWMyMjM2YWEzMzBlYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gZ6Prx_Yw3d2nTgGDPOJqsgsVFUfU-Pb5mwjUbkN8-I'
     }
+  };
+
+
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  }
 
   useEffect(()=>{
-    fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
-    .then(res => res.json())
-    .then(res => setApiData(res.results || []))
-    .catch(err => console.error(err));
+    const ls = localStorage.getItem('titles');
+
+    if(ls){
+      setApiData(JSON.parse(ls) || []);
+    }
+  },[])
+
+  useEffect(() => {
+    // fetch(`https://api.themoviedb.org/3/movie/${category ? category : "now_playing"}?language=en-US&page=1`, options)
+    //   .then(res => res.json())
+    //   .then(res => setApiData(res.results || []))
+    //   .catch(err => console.error(err));
+
+    async function fetchData() {
+      try {
+        const response = await fetch(`https://api.themoviedb.org/3/movie/${category ? category : "now_playing"}?language=en-US&page=1`, options);
+        const result = await response.json();
+
+        localStorage.setItem('titles', JSON.stringify(result.results));
+
+        setApiData(result.results || []);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchData();
 
     const currentRef = cardsRef.current;
     currentRef.addEventListener('wheel', handleWheel);
@@ -38,11 +61,11 @@ const TitleCards = ({title, category}) => {
     <div className='title-cards'>
       <h2>{title ? title : "Popular on Netflix"} </h2>
       <div className="card-list" ref={cardsRef}>
-        {apiData.map((card, index) =>{
+        {apiData.map((card, index) => {
           return <div className="card" key={index}>
 
             <img src={
-              card.backdrop_path ? `https://image.tmdb.org/t/p/w500`+card.backdrop_path : 'assets/fallback.png' } alt = {card.original_title}
+              card.backdrop_path ? `https://image.tmdb.org/t/p/w500` + card.backdrop_path : 'assets/fallback.png'} alt={card.original_title}
             />
 
             <p>{card.original_title}</p>
