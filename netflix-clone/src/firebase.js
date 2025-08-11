3982c8eec30ee731cf849cbb9bf76c9f51e8f141
BrajeshPrajapati003 <prajapatibrajesh003@gmail.com>
2025-08-11 17:17:19 +0530

    Added Loading Animation

--- netflix-clone/src/firebase.js
+++ netflix-clone/src/firebase.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import { addDoc, collection, getFirestore } from 'firebase/firestore';
 import { getAnalytics } from "firebase/analytics";
+import { toast } from 'react-toastify'
 
 
 const firebaseConfig = {
@@ -35,7 +36,7 @@ const signup = async (name, email, password)=> {
         });
     }catch (error){
         console.log(error);
-        alert(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
         
     }
 }
@@ -46,7 +47,7 @@ const login = async (email, password) =>{
         await signInWithEmailAndPassword(auth, email, password);
     }catch(error){
         console.log(error);
-        alert(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
         
     }
 }
